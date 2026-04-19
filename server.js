b59950869e0afb45f2d54bcd36a40b2343cd4210
ma7mo4d <ma7mo4dsaeed74@gmail.com>
2026-04-19 02:49:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ ${message}
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
